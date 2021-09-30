c1a3db3af471474be10e98231184e1f6747745e3
Peter roe <peterroe@163.com>
2021-09-30 13:47:22 +0800

    :heavy_plus_sign: jest

--- webpack.config.js
+++ webpack.config.js
@@ -3,14 +3,17 @@
  * @Author: 林舒恒
  * @Date: 2021-09-30 12:07:43
  * @LastEditors: 林舒恒
- * @LastEditTime: 2021-09-30 12:58:38
+ * @LastEditTime: 2021-09-30 13:44:32
  */
 const path = require('path')
-
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 module.exports = {
     entry: './src/main.js',
     output: {
         path: path.resolve(__dirname, 'dist')
     },
-    mode: 'development'
+    mode: 'development',
+    plugins: [
+        new CleanWebpackPlugin()
+    ]
 }
