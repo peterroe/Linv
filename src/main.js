359ef084603935a2fa422aa2827dae846e6656fb
Peter roe <peterroe@163.com>
2021-09-30 13:43:26 +0800

    :sparkles: Lin

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@
  * @Author: 林舒恒
  * @Date: 2021-09-30 12:04:07
  * @LastEditors: 林舒恒
- * @LastEditTime: 2021-09-30 12:58:21
+ * @LastEditTime: 2021-09-30 13:41:16
  */
-function Lin() {
-    console.log(1)
+function Lin({
+    template,
+    el
+}) {
+    let dom = document.querySelector(el)
+    dom.innerHTML = template
 }
 
 export default Lin
+
+/* usage:
+    new Lin({
+        template: ``,
+        el: '#app'
+    })
+*/
