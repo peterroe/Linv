4954dcad4a15f6aba456368486da957d268c1766
Peter roe <peterroe@163.com>
2021-09-30 12:05:16 +0800

    src

--- push.sh
+++ push.sh
@@ -3,8 +3,8 @@
  # @Author: 林舒恒
  # @Date: 2021-09-30 11:39:13
  # @LastEditors: 林舒恒
- # @LastEditTime: 2021-09-30 11:59:29
+ # @LastEditTime: 2021-09-30 12:05:05
 ### 
 # eval 'git add .'
-eval 'git commit -am ${1}'
-eval 'git push origin master'
+git commit -am ${1}
+git push origin master
