e0ba91c5b485613acdf97a25195e12bf5e815f24
Peter roe <peterroe@163.com>
2021-09-30 12:02:00 +0800

    :sparkles: feat:ignore

---
+++ push.sh
@@ -0,0 +1,10 @@
+###
+ # @Descripttion: 
+ # @Author: 林舒恒
+ # @Date: 2021-09-30 11:39:13
+ # @LastEditors: 林舒恒
+ # @LastEditTime: 2021-09-30 11:59:29
+### 
+# eval 'git add .'
+eval 'git commit -am ${1}'
+eval 'git push origin master'
